ae064acbbd489cd2d98fdab2cc02819fb2924649
Michael Reiersgaard <michael.reiersgaard@gmail.com>
2021-06-02 16:35:49 -0700

    add image changing feature

--- js/scripts.js
+++ js/scripts.js
@@ -24,33 +24,45 @@ $(document).ready(function() {
     
     if (height > 36) {
       $("#ride1").addClass("can-ride");
+      $("#ride1img").attr("src","../images/can-ride.png");
     } else {
       $("#ride1").addClass("cant-ride");
+      $("#ride1img").attr("src","../images/cant-ride.png");
       }
     if (height < 48) {
       $("#ride2").addClass("can-ride");
+      $("#ride2img").attr("src","../images/can-ride.png");
     } else {
       $("#ride2").addClass("cant-ride");
+      $("#ride2img").attr("src","../images/cant-ride.png");
       }
     if (height >= 48 && height <= 60) {
       $("#ride3").addClass("can-ride");
+      $("#ride3img").attr("src","../images/can-ride.png");
     } else {
       $("#ride3").addClass("cant-ride");
+      $("#ride3img").attr("src","../images/cant-ride.png");
       }
     if (height > 60) {
       $("#ride4").addClass("can-ride");
+      $("#ride4img").attr("src","../images/can-ride.png");
     } else {
       $("#ride4").addClass("cant-ride");
+      $("#ride4img").attr("src","../images/cant-ride.png");
       }
     if (height >= 48 && height <= 72) {
       $("#ride5").addClass("can-ride");
+      $("#ride5img").attr("src","../images/can-ride.png");
     } else {
       $("#ride5").addClass("cant-ride");
+      $("#ride5img").attr("src","../images/cant-ride.png");
       }
     if (height < 66) {
       $("#ride6").addClass("can-ride");
+      $("#ride6img").attr("src","../images/can-ride.png");
     } else {
       $("#ride6").addClass("cant-ride");
+      $("#ride6img").attr("src","../images/cant-ride.png");
       }
     $("#output").show();
   
@@ -58,7 +70,7 @@ $(document).ready(function() {
     if (height < 48) {
       $("#rudeHeight").text("shorty");
     } else if (height >= 48 && height <= 72) {
-      $("#rudeHeight").text("you completely average person");
+      $("#rudeHeight").text("you completely average person. You're not special");
     } else {
       $("#rudeHeight").text("skyscraper");
     }
@@ -68,6 +80,7 @@ $(document).ready(function() {
   $("button#reset").click(function() {
     $("#ride1, #ride2, #ride3, #ride4, #ride5, #ride6").removeClass();
     $("#output").hide();
+    ("#ride1img, #ride2img, #ride3img, #ride4img, #ride5img, ").attr("src","../images/can-ride.png")
   });
   
 
