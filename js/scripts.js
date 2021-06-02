02e9629950b1e04d522f1bf3228412ef7752daf3
Michael Reiersgaard <michael.reiersgaard@gmail.com>
2021-06-02 16:02:37 -0700

    add ride logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,58 @@
-function height(number1, number2) {
+function heightInIn(number1, number2) {
+console.log(number1+" and "+number2)
   return (number1 * 12) + number2;
 }
 
 
 
-$(document).ready(function(){
-  $().submit(function(event) {
-    const number1 = parseInt(input("#feet"));
-    const number2 = parseInt(input("#inches"));
-    const height = height(number1, number2);
-    $(".entered-height").slideToggle();
+$(document).ready(function() {
+  $("form#heightCalc").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#feet").val());
+    let number2 = parseInt($("#inches").val());
+    if (number1 < 1 || isNaN(number1)) {
+      number1 = 0;
+    }
+    if (number2 < 1 || isNaN(number2)) {
+      number2 = 0;
+    }
+    const height = heightInIn(number1, number2);
+    $("#userFeet").text(number1);
+    $("#userInches").text(number2);
+
+    $("#ride1, #ride2, #ride3, #ride4, #ride5, #ride6").removeClass();
+    
+    if (height > 36) {
+      $("#ride1").addClass("can-ride");
+    } else {
+      $("#ride1").addClass("cant-ride");
+      }
+    if (height < 48) {
+      $("#ride2").addClass("can-ride");
+    } else {
+      $("#ride2").addClass("cant-ride");
+      }
+    if (height >= 48 && height <= 60) {
+      $("#ride3").addClass("can-ride");
+    } else {
+      $("#ride3").addClass("cant-ride");
+      }
+    if (height > 60) {
+      $("#ride4").addClass("can-ride");
+    } else {
+      $("#ride4").addClass("cant-ride");
+      }
+    if (height >= 48 && height <= 72) {
+      $("#ride5").addClass("can-ride");
+    } else {
+      $("#ride5").addClass("cant-ride");
+      }
+    if (height < 66) {
+      $("#ride6").addClass("can-ride");
+    } else {
+      $("#ride6").addClass("cant-ride");
+      }
+    $("#output").show();
 
 
 
@@ -17,6 +60,12 @@ $(document).ready(function(){
 
 
 
+  });
+  $("button#reset").click(function() {
+    $("#ride1, #ride2, #ride3, #ride4, #ride5, #ride6").removeClass();
+    $("#output").hide();
+  });
+  
+  //Insult text
 
-  }
-}
+});
